3b2edbadd9d4b7decce3004b0bc68e28aa38b05b
rgulle4 <rgulle4@lsu.edu>
2016-09-07 08:15:15 -0500

    Refactor: separate packages for mvc

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/view.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
