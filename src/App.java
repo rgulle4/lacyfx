9dcb3bf78002a152011f1eb682f08b1188e4f993
rgulle4 <rgulle4@lsu.edu>
2016-09-14 23:04:53 -0500

    Change default size to 800x600

--- src/App.java
+++ src/App.java
@@ -10,7 +10,7 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("views/view.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
