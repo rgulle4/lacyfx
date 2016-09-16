9694db8f33f8b0f9af9c45dcc9791dd41a1f9455
rgulle4 <rgulle4@lsu.edu>
2016-09-16 02:00:56 -0500

    Add basic autosum functionality to env weights

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,4 +1,49 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    private int SLEEP_LENGTH = 2;
+
+    @FXML private TextField envSum;
+    @FXML private TextField gwpWeightTxt;
+    @FXML private TextField odpWeightTxt;
+    @FXML private TextField apWeightTxt;
+    @FXML private TextField pocpWeightTxt;
+
+    public void envWeightsListener(Event actionEvent) throws InterruptedException {
+        changeEnvSum();
+    }
+
+    private void changeEnvSum() throws InterruptedException {
+        Thread.sleep(SLEEP_LENGTH);
+        double sum = txtToDouble(gwpWeightTxt)
+              + txtToDouble(odpWeightTxt)
+              + txtToDouble(apWeightTxt)
+              + txtToDouble(pocpWeightTxt);
+        Thread.sleep(SLEEP_LENGTH);
+        envSum.setText(String.valueOf(sum));
+    }
+
+    private double txtToDouble(TextField textField) {
+        String str = textField.getText();
+        if (str.isEmpty() || isNotNumeric(str)) {
+            return 0;
+        } else {
+            return Double.parseDouble(str);
+        }
+
+    }
+
+    private boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+
+    private boolean isNotNumeric(String str) {
+        return !(isNumeric(str));
+    }
 }
