77933250ebd8931ed1eff78646e8c874edda86c3
rgulle4 <rgulle4@lsu.edu>
2016-09-20 00:45:28 -0500

    Add EPDDatabaseTest stub
    - The EPDDatabase class itself is a stub.
    - All of this is preliminary... just trying to get testing started.

---
+++ test/models/EPDDatabaseTest.java
@@ -0,0 +1,18 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EPDDatabaseTest {
+
+    @Test
+    public void testGetResultsFilteredBy() throws Exception {
+
+    }
+
+    @Test
+    public void testGetResults() throws Exception {
+
+    }
+}
