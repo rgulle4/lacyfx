ff6a2f0c15ff01535d2fba0e2160f9b7fa074158
rgulle4 <rgulle4@lsu.edu>
2016-09-21 02:19:25 -0500

    Add Design, Layer, Material (STOPPING POINT WIP).
    This needs a lot of work.
    
    - Is the composition right? A design has a number of layers. Each layer
      has a thickness, and one material. Each material is basically a row in
      the EPD database.
    
    - The calculations need to be listed out.

--- src/models/Design.java
+++ src/models/Design.java
@@ -1,7 +1,40 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * A design is a pavement design, consisting of a number of layers.
+ * A design is a pavement design, consisting of a number of layers. Each layer
+ * has a thickness, and a material.
  */
 public class Design {
+
+    /* -- Fields ----------------------------------------------- */
+
+    private ArrayList<Layer> layers;
+
+    /* -- Constructor(s) --------------------------------------- */
+
+    /**
+     * Constructs an empty design.
+     */
+    public Design() {
+
+    }
+
+    /* -- Method(s) -------------------------------------------- */
+
+    /**
+     * Add layer to the top of the design.
+     * @return true if add is successful, false otherwise.
+     */
+    public boolean addLayer() {
+        return true;
+    }
+
+    /* -- Calculation(s) --------------------------------------- */
+
+    // TODO: add all the necessary calculations; need clarification on this.
+
+
 }
