27ae47a4fb285eff1a6aa7a9a87f41001ad153ea
Qiandong Nie <nqd1230@gmail.com>
2016-09-28 08:40:14 -0500

    update addfile function

--- src/cm/App.java
+++ src/cm/App.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class App extends Application {
 
@@ -14,6 +17,13 @@ public class App extends Application {
         primaryStage.setTitle("lacyfx WIP");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+    }
+    private static Stage Stage;
+    public static void showfile(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Load designed file");
+        File file = fileChooser.showOpenDialog(Stage);
+
     }
 
 
--- src/cm/controllers/ImportTabController.java
+++ src/cm/controllers/ImportTabController.java
@@ -1,7 +1,17 @@
 package cm.controllers;
 
+import cm.App;
+import javafx.fxml.FXML;
+
 /**
  * Created by royg59 on 9/21/16.
  */
 public class ImportTabController {
+    private App main;
+
+    @FXML
+    private void addfile(){
+
+        main.showfile();
+    }
 }
