2df10baf7b03224f2170ebfe5dc20fbd442150c7
Qiandong Nie <nqd1230@gmail.com>
2016-09-29 14:13:13 -0500

    Achieve fill CS_Textfiled for searchButton

--- src/cm/controllers/LoadMaterialController.java
+++ src/cm/controllers/LoadMaterialController.java
@@ -2,6 +2,7 @@ package cm.controllers;
 
 import cm.models.EPDDatabase;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import libs.net.efabrika.util.DBTablePrinter;
 
 import java.sql.ResultSet;
@@ -14,8 +15,14 @@ public class LoadMaterialController {
 
     // To DO: select alternative materials from database for calculating and comparing
     @FXML
+    private TextField TF_CS;
+
+    @FXML
     private void searchbutton() throws SQLException {
-        ResultSet r = new EPDDatabase().getResultsFilteredBy("CS >= '7000'");
+        StringBuilder CS_field = new StringBuilder("CS >= ");
+        String CS_Textfiled = CS_field.append("'").append(TF_CS.getText()).append("'").toString();
+
+        ResultSet r = new EPDDatabase().getResultsFilteredBy(CS_Textfiled);
         DBTablePrinter.printResultSet(r);
     }
 }
