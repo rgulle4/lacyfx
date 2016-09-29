a151c5b42753113cad62b522625d0102cebf0238
Qiandong Nie <nqd1230@gmail.com>
2016-09-29 11:18:56 -0500

    Activate SearchButton in the LoadMaterial Window to connect with Database

--- src/cm/controllers/LoadMaterialController.java
+++ src/cm/controllers/LoadMaterialController.java
@@ -1,9 +1,21 @@
 package cm.controllers;
 
+import cm.models.EPDDatabase;
+import javafx.fxml.FXML;
+import libs.net.efabrika.util.DBTablePrinter;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by Administrator on 2016/9/28.
  */
 public class LoadMaterialController {
 
-    // To DO: select alternative materials from database for calculating and comparing 
+    // To DO: select alternative materials from database for calculating and comparing
+    @FXML
+    private void searchbutton() throws SQLException {
+        ResultSet r = new EPDDatabase().getResultsFilteredBy("CS >= '7000'");
+        DBTablePrinter.printResultSet(r);
+    }
 }
