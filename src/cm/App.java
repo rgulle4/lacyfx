d061cbee973dad475f200bd827c42f7b726896f4
Qiandong Nie <nqd1230@gmail.com>
2016-09-28 21:54:29 -0500

    activate load material button

--- src/cm/App.java
+++ src/cm/App.java
@@ -4,13 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class App extends Application {
-
+    private static Stage primaryStage;
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("views/view.fxml"));
@@ -18,15 +21,28 @@ public class App extends Application {
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
-    private static Stage Stage;
+
     public static File showfile(){
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Load designed file");
-        File file = fileChooser.showOpenDialog(Stage);
+        File file = fileChooser.showOpenDialog(primaryStage);
         return file;
-
     }
 
+    public static void showLoadMaterial() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(App.class.getResource("views/loadMaterial_window.fxml"));
+        AnchorPane addNewMaterial = loader.load();
+
+        Stage addDialogStage = new Stage();
+        addDialogStage.setTitle("Add New Material");
+        addDialogStage.initModality(Modality.WINDOW_MODAL);
+        addDialogStage.initOwner(primaryStage);
+
+        Scene scene = new Scene(addNewMaterial);
+        addDialogStage.setScene(scene);
+        addDialogStage.showAndWait();
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/cm/controllers/LayerInformationController.java
+++ src/cm/controllers/LayerInformationController.java
@@ -1,7 +1,19 @@
 package cm.controllers;
 
+import cm.App;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 /**
  * Created by royg59 on 9/21/16.
  */
 public class LayerInformationController {
+
+    private App main;
+
+    @FXML
+    private void LoadMatBtn() throws IOException {
+        main.showLoadMaterial();
+    }
 }
