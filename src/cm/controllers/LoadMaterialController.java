7e31bee42bd959a105797568636e90c1958a31b3
Qiandong Nie <nqd1230@gmail.com>
2016-09-29 08:39:15 -0500

    add LoadMaterialController

---
+++ src/cm/controllers/LoadMaterialController.java
@@ -0,0 +1,9 @@
+package cm.controllers;
+
+/**
+ * Created by Administrator on 2016/9/28.
+ */
+public class LoadMaterialController {
+
+    // To DO: select alternative materials from database for calculating and comparing 
+}
