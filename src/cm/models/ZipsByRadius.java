290fdccc494c105475adbd953e7d3507eda19288
rgulle4 <rgulle4@lsu.edu>
2016-10-17 22:56:01 -0500

    Add WIP ZipsByRadius.java rf iss #5
    - Just a bunch of WIP scribble scratch, but `ZipsByRadius.main()`
      shows an example call to the Google Maps API, asking the distance
      from one zip code to another (70831 to 70803).
    
    - TODO: find a place to put this functionality, figure out what all's
      needed, etc. But this is a start.
    
    - Misc wild thought: maybe we can "pre-build" a big table of all
      possible zip codes (say, in LA, TX, MS, AR, etc), and the distances
      between them. How many are there? This would obviate the need for
      internet connectivity, and also make the search faster. But idk, I
      need some sleep right now.

---
+++ src/cm/models/ZipsByRadius.java
@@ -0,0 +1,109 @@
+package cm.models;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+/*
+ * STOPPING POINT: see tabs list in 00notes/TODO-urls.txt
+ */
+
+
+/**
+ * Ideally, will provide a getZipsByRadius method that returns a list of zip
+ * codes within a given distance from a given destination zip code.
+ *
+ * TODO: Google maps API seems simplest... Still need to explore the different
+ * things we can do with it. We'll use Gson to convert GET responses to a java
+ * objects.
+ *
+ * EXAMPLE REQUEST to get distance-by-car between two zip codes...
+ * GET https://maps.googleapis.com/maps/api/distancematrix/json?origins=70831&destinations=70803&mode=car&sensor=false
+ * Response:
+ *   {
+ *     "destination_addresses": [
+ *       "Baton Rouge, LA 70803, USA"
+ *     ],
+ *     "origin_addresses": [
+ *       "Baton Rouge, LA 70831, USA"
+ *     ],
+ *     "rows": [
+ *       {
+ *         "elements": [
+ *           {
+ *             "distance": {
+ *               "text": "7.4 km",
+ *               "value": 7381
+ *             },
+ *             "duration": {
+ *               "text": "16 mins",
+ *               "value": 960
+ *             },
+ *             "status": "OK"
+ *           }
+ *         ]
+ *       }
+ *     ],
+ *     "status": "OK"
+ *   }
+ */
+public class ZipsByRadius {
+
+    /* -- field(s) ---------------------------------------------------- */
+    private String destinationZip;
+    private Double radius; // Radius filter, in miles.
+    private ArrayList<String> zips;
+
+    /* -- constructor(s) ---------------------------------------------- */
+
+    private ZipsByRadius() { /* */ }
+
+    public ZipsByRadius(String destinationZip,
+                        Double radius) {
+        this.destinationZip = destinationZip;
+        this.radius = radius;
+    }
+
+    /* -- methods) ---------------------------------------------------- */
+
+    public ArrayList<String> getZipsByRadius() {
+
+        return zips;
+    }
+
+    /* -- misc method(s) ---------------------------------------------- */
+
+    /**
+     * Main method, used just for testing.
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
+        String destinationZip = "70803";
+        Double radius = 10.0; // <-- not sure how to use this yet
+        String sourceZip1 = "70831";
+
+        StringBuilder sbRequestUrl = new StringBuilder();
+        StringBuilder sbResult = new StringBuilder();
+        URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?mode=car&sensor=false" +
+                          "&origins=" + sourceZip1 +
+                          "&destinations=" + destinationZip);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null)
+            sbResult.append(line);
+        rd.close();
+        conn.disconnect();
+        System.out.println(sbResult.toString());
+    }
+
+
+}
