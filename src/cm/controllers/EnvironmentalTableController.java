8934465c188d3054f53e2b184f64f27efa1e1fb2
Qiandong Nie <nqd1230@gmail.com>
2016-11-11 07:38:59 -0600

    set up method to control elements in the EnvDataTable.fxml

--- src/cm/controllers/EnvironmentalTableController.java
+++ src/cm/controllers/EnvironmentalTableController.java
@@ -1,8 +1,19 @@
 package cm.controllers;
 
+import cm.models.Design;
+import cm.models.Layer;
+import cm.models.Mix;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static cm.models.Model.DESIGNS;
 
 /**
  * Created by Administrator on 2016/11/10.
@@ -10,10 +21,87 @@ import javafx.scene.control.TableView;
 public class EnvironmentalTableController {
     @FXML
     private TableView dataTable;
-
-
-
-    public void showData(){
+    @FXML
+    private ComboBox performanceType_ComboBox;
+    @FXML
+    private ComboBox environmentalImpact_ComboBox;
+    @FXML
+    private ComboBox rawValue_ComboBox;
+    @FXML
+    private ComboBox design_ComboBox;
+    @FXML
+    private ComboBox layer_ComboBox;
+    @FXML
+    private ComboBox mix_ComboBox;
+    @FXML
+    private ComboBox impactCategory_ComboBox;
+    // Type of performance
+    ObservableList<String> performanceType = FXCollections.
+            observableArrayList(
+                    "Environmental Performance",
+                    "Economical Performance",
+                    "Overall Performance");
+    // EnvironmentalImpact type
+    ObservableList<String> impactType = FXCollections.
+            observableArrayList(
+                    "EPD",
+                    "TSP",
+                    "Overall"
+            );
+    // raw Value type
+    ObservableList<String> rawValueType = FXCollections.
+            observableArrayList(
+                    "Raw impact per functional unit",
+                    "Normalized impact per functional unit",
+                    "Weighted impact per functional unit"
+            );
+    // Number of designs
+    ObservableList<String> designNum = FXCollections.
+            observableArrayList();
+    // Number of layers in this design
+    ObservableList<String> layerNum = FXCollections
+            .observableArrayList();
+    // Number of layers in this design
+    ObservableList<String> mixNum = FXCollections
+            .observableArrayList();
+    @FXML
+    public void initialize() {
+        //set up default elements
+        setupPerformanceType_ComboBox();
+        setupEnvironmentalImpact_ComboBox();
+        setupRawValue_ComboBox();
+        setupImpactCategory_ComboBox();
+        //set up design number
+        setupDesignNumber_ComboBox();
+    }
+    private String perfType;
+    private String envImpactType;
+    private String valueType;
+    private String impactCategory;      //GWP, ODP, AP, EP, POCP, Energy Consumption...
+    public void showOutputTable(){
+        String designID = design_ComboBox.getSelectionModel().getSelectedItem().toString();
+        String layerID = layer_ComboBox.getSelectionModel().getSelectedItem().toString();
+        Design designTemp = DESIGNS.get(designID);
+        int layerIndex = layer_ComboBox.getSelectionModel().getSelectedIndex();
+        Layer layerTemp = designTemp.getLayer(layerIndex);
+        int mixIndex = mix_ComboBox.getSelectionModel().getSelectedIndex();
+        //Get the whole selected Mix lists
+        List<Mix> mixsTemp = new ArrayList<Mix>();
+        String selectedMixItem = mix_ComboBox.getValue().toString();
+        if(selectedMixItem == "All Mixes"){
+            mixsTemp = layerTemp.getMixes();
+        }else{
+            Mix selectedMix = layerTemp.getMaterial(mixIndex);
+            mixsTemp.add(selectedMix);
+        }
+        perfType = performanceType_ComboBox.getSelectionModel().getSelectedItem().toString();
+        envImpactType = environmentalImpact_ComboBox.getSelectionModel().getSelectedItem().toString();
+        valueType = rawValue_ComboBox.getSelectionModel().getSelectedItem().toString();
+        impactCategory = impactCategory_ComboBox.getSelectionModel().getSelectedItem().toString();
+        String alternative_ID = new StringBuilder(designID).append(layerID).toString();
+        showData(alternative_ID,mixsTemp);
+    }
+    public void showData(String alternative_id,List<Mix> mixes){
         addCertainNumTableColumns();
         insertValueToTableView();
     }
@@ -29,6 +117,103 @@ public class EnvironmentalTableController {
         insertValueToColumns();
     }
     private void insertValueToColumns(){
+    }
+    private void setupPerformanceType_ComboBox(){
+        performanceType_ComboBox.setItems(performanceType);
+        performanceType_ComboBox.setValue(performanceType.get(0));
+    }
 
+    private void setupEnvironmentalImpact_ComboBox(){
+        environmentalImpact_ComboBox.setItems(impactType);
+        environmentalImpact_ComboBox.setValue(impactType.get(0));
+    }
+
+    private void setupRawValue_ComboBox(){
+        rawValue_ComboBox.setItems(rawValueType);
+        rawValue_ComboBox.setValue(rawValueType.get(0));
+    }
+
+    public void setupImpactCategory_ComboBox(){
+        valueType = rawValue_ComboBox.getSelectionModel().getSelectedItem().toString();
+        // Impact Category
+        ObservableList<String> impactCategoryName =FXCollections
+                .observableArrayList(
+                        "Impact Analysis Comparison of All Alternatives",
+                        "Impact Analysis per Alternative",
+                        "GWP","ODP","AP","EP", "POCP",
+                        "PrimaryEnergyConsumption"
+                );
+        if(valueType == "Weighted impact per functional unit"){
+            impactCategory_ComboBox.setItems(impactCategoryName);
+        }else{
+            impactCategoryName.remove(0);
+            impactCategory_ComboBox.setItems(impactCategoryName);
+        }
+
+    }
+
+    private void setupDesignNumber_ComboBox(){
+        for (String dKey:DESIGNS.keySet()){
+            designNum.add(dKey);
+        }
+        if (DESIGNS.size()>0){
+            designNum.addAll("All designs");
+        }
+        design_ComboBox.setItems(designNum);
+        design_ComboBox.setValue(designNum.get(0));
+    }
+
+    public void setupLayerNumber_ComboBox(){
+        // clean items in the layerNumber_ComboBox
+        layerNum.clear();
+        if (!design_ComboBox.getSelectionModel().isEmpty()){
+            String selectedDeisgnKey = design_ComboBox.getValue().toString();
+            int layerNumber = DESIGNS.get(selectedDeisgnKey).getNumberOfLayers();
+            if(layerNumber>0){
+                for (int i = 1; i<= layerNumber;i++){
+                    StringBuilder sb = new StringBuilder("Layer ");
+                    String layerName= sb.append(i).toString();
+                    layerNum.add(layerName);
+                }
+                layerNum.addAll("All layers");
+                layer_ComboBox.setItems(layerNum);
+            }
+            else {
+                System.out.println("No layer was added for this design!!");
+            }
+        }
+        else{
+            System.out.println("Select a certain design first!!");
+        }
+
+    }
+
+    public void setupMixNum_ComboBox(){
+        // clean MixNum_ComBox first
+        mixNum.clear();
+        if(!design_ComboBox.getSelectionModel().isEmpty()){
+            String selectedDeisgnKey = design_ComboBox.getValue().toString();
+            if(!layer_ComboBox.getSelectionModel().isEmpty()){
+                int layerOfIndex = layer_ComboBox.getSelectionModel().getSelectedIndex();
+                int mixNumber = DESIGNS.get(selectedDeisgnKey)
+                        .getLayer(layerOfIndex)
+                        .getNumberofMaterials();
+                if(mixNumber > 0){
+                    for (int i = 1; i <= mixNumber;i ++){
+                        StringBuilder sb = new StringBuilder("Mix ");
+                        String mixName = sb.append(i).toString();
+                        mixNum.add(mixName);
+                    }
+                    mixNum.addAll("All Mixes");
+                    mix_ComboBox.setItems(mixNum);
+                }else{
+                    System.out.println("No mix was added!!");
+                }
+            }else{
+                System.out.println("Select a certain layer first!!");
+            }
+        }else{
+            System.out.println("Select a certain design first!!");
+        }
     }
 }
